9588b5225cf9ddaf67b232042250f03c79b8da3c
Mariano Guerra <luismarianoguerra@gmail.com>
2013-03-14 17:30:36 +0100

    update clojure dep to 1.5.1

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :url "http://github.com/marianoguerra/pipe"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]])
